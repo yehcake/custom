23dbcecc56ffb62641731e6193eb7ceff9b407a3
yehcake <developer@yehcake.com>
2014-11-14 12:33:26 +0000

    added order process image to faq

--- faq/index.php
+++ faq/index.php
@@ -20,8 +20,11 @@
                     <div data-role="collapsible">
                         <h3>如何確認訂單？ How do I book an order?</h3>
                         <p>
-                            我們在接到<a href="/availability/">訂單</a>的72小時之內會與您確認，如果您的訂單金額為NT3,000元以上，在確認（正式下單）前我們會請您先付訂金。
-                            We will respond to your <a href="/availability/">inquiry</a> within 72-hours of receipt. If your order is NT3,000 or more, we request a deposit before booking your order.
+                            <img src="../images/orderProcess.png" border="0" width="168" height="484" orgWidth="168" orgHeight="484" usemap="#orderProcessMap" alt="[ 訂單流程 Order Process ]" />
+                            <map name="orderProcessMap">
+                                <area href="/availability/" shape="rect" coords="0,0,168,72" style="outline:none;" />
+                                <area href="/availability/" shape="rect" coords="0,201,168,278" style="outline:none;" />
+                            </map>
                         </p>
                     </div>
                     <div data-role="collapsible">
