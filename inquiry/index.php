2e243cc98cbf1cd2058159b658cca803e720e188
yehcake <developer@yehcake.com>
2014-12-04 05:55:56 +0000

    added RFQ page

---
+++ inquiry/index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html>
+
+<head>
+    <title>
+        Request For Quotation |
+        <?php include "../stubs/head.php";?>
+</head>
+
+<body>
+    <div data-role="page">
+
+        <div role="main" class="ui-content no-top-padding">
+
+            <?php include "../stubs/header.php";?>
+
+            <div class="content-container">
+                <script type="text/javascript" src="https://formscentral.acrobat.com/Clients/Current/FormsCentral/htmlClient/scripts/adobe.form.embed.min.js"></script>
+                <script type="text/javascript">
+                    ;
+                    ADOBEFORMS.EmbedForm({
+                        formId: "OyRjBrn5tY27GOdkBktorA"
+                    });
+                </script>
+            </div>
+
+            <?php include "../stubs/footer.php";?>
+
+        </div>
+        <!-- /content -->
+    </div>
+    <!-- /page -->
+</body>
+
+</html>
