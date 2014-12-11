f60852d8990b3d37003456d3e406aca4c14a90ee
yehcake <developer@yehcake.com>
2014-12-11 05:08:20 +0000

    added PDF file

--- inquiry/index.php
+++ inquiry/index.php
@@ -15,13 +15,9 @@
             <?php include "../stubs/header.php";?>
 
             <div class="content-container">
-                <script type="text/javascript" src="https://formscentral.acrobat.com/Clients/Current/FormsCentral/htmlClient/scripts/adobe.form.embed.min.js"></script>
-                <script type="text/javascript">
-                    ;
-                    ADOBEFORMS.EmbedForm({
-                        formId: "OyRjBrn5tY27GOdkBktorA"
-                    });
-                </script>
+                <p>Upon submission, one of our cake artists will review the request and respond to the customer by e-mail within 72-hours.  Reference photos may be emailed to <a href="mailto:info@yehcake.com">info@yehcake.com</a> or LINE-ed to yehcake.  Also, don't forget the <a href="/pricing">pricing calculator</a>.</p>
+                <a href="rfq_v1.pdf" class="ui-btn ui-btn-inline ui-corner-all" target="_blank">Download Form</a>
+                <p>※ We recommend you download the form (as opposed to viewing it within the browser) and work with it from your desktop.  The latest form is Version 1.</p>
             </div>
 
             <?php include "../stubs/footer.php";?>
