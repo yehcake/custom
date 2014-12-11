90a11ac9ab432ee4f8f368b226691702aad80f66
yehcake <developer@yehcake.com>
2014-12-11 05:18:21 +0000

    wording change for inquiry page

--- inquiry/index.php
+++ inquiry/index.php
@@ -15,7 +15,7 @@
             <?php include "../stubs/header.php";?>
 
             <div class="content-container">
-                <p>Upon submission, one of our cake artists will review the request and respond to the customer by e-mail within 72-hours.  Reference photos may be emailed to <a href="mailto:info@yehcake.com">info@yehcake.com</a> or LINE-ed to yehcake.  Also, don't forget the <a href="/pricing">pricing calculator</a>.</p>
+                <p>Upon submission, one of our cake artists will review the request and respond to the customer by e-mail within 72-hours.  Reference photos may be emailed to <a href="mailto:info@yehcake.com">info@yehcake.com</a> or LINE-ed to yehcake.  Also, please don't forget to (1) <a href="/availability">check availability</a> as well as (2) provide a <a href="/pricing">pricing estimate</a> to the customer.</p>
                 <a href="rfq_v1.pdf" class="ui-btn ui-btn-inline ui-corner-all" target="_blank">Download Form</a>
                 <p>※ We recommend you download the form (as opposed to viewing it within the browser) and work with it from your desktop.  The latest form is Version 1.</p>
             </div>
