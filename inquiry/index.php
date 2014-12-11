143d12fa56f4a343af4c6cb317563ea097451588
yehcake <developer@yehcake.com>
2014-12-11 06:57:56 +0000

    more wording change

--- inquiry/index.php
+++ inquiry/index.php
@@ -18,7 +18,7 @@
                 <p>Upon submission, one of our cake artists will review the request and respond to the customer by e-mail within 72-hours.  Reference photos may be emailed to <a href="mailto:info@yehcake.com">info@yehcake.com</a> or LINE-ed to yehcake.</p>
                 <p>Also, please don't forget to (1) <a href="/availability">check availability</a> as well as (2) provide a <a href="/pricing">pricing estimate</a> to the customer.</p>
                 <a href="https://app.box.com/s/zj91if0hdv3jha01l4r0" class="ui-btn ui-btn-inline ui-corner-all">Get Form</a>
-                <p>※ We recommend you download the form and work with it from your desktop.  It may not function correctly if used from within the browser.</p>
+                <p>※ We recommend you download the form and work with it from your desktop.  It will not function correctly if used from within the browser.</p>
             </div>
 
             <?php include "../stubs/footer.php";?>
