cc79946cb091d6d1a67c90340cc7c773ef37e071
yehcake <developer@yehcake.com>
2015-06-10 04:09:12 +0000

    changed phone order form

--- inquiry/index.php
+++ inquiry/index.php
@@ -17,8 +17,7 @@
             <div class="content-container">
                 <p>Upon submission, one of our cake artists will review the request and respond to the customer by e-mail within 72-hours.  Reference photos may be emailed to <a href="mailto:info@yehcake.com">info@yehcake.com</a> or LINE-ed to yehcake.</p>
                 <p>Also, please don't forget to (1) <a href="/availability">check availability</a> as well as (2) provide a <a href="/pricing">pricing estimate</a> to the customer.</p>
-                <a href="https://app.box.com/s/zj91if0hdv3jha01l4r0" class="ui-btn ui-btn-inline ui-corner-all">Get Form</a>
-                <p>※ We recommend you download the form and work with it from your desktop.  It will not function correctly if used from within the browser.</p>
+                <a href="https://docs.google.com/forms/d/1suCXKtHjjqHgIuDE_ej45krn0qixQDyElxPMpfJ66G8/viewform" class="ui-btn ui-btn-inline ui-corner-all">Continue</a>
             </div>
 
             <?php include "../stubs/footer.php";?>
